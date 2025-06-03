5801741bb911ad0f3e7d8bb74c1635392bb75bd6
AndreaGuzman9618 <guzmangarciaallyssonandrea@gmail.com>
2025-06-03 15:56:45 -0500

    Corrección de stepper

--- Act_2/Assets/index.js
+++ Act_2/Assets/index.js
@@ -2,24 +2,56 @@ const prevBtns = document.querySelectorAll(".btn-prev");
 const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-setp");
 
 let formStepsNum = 0;
 
-nextBtns.forEach(btn =>{
-    btn.addEventListener("click", ()=>{
-        formSteps++;
-        updateFormSteps();
+nextBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+        e.preventDefault();
+        if (formStepsNum < formSteps.length - 1) {
+            formStepsNum++;
+            updateFormSteps();
+            updateProgressBar();
+        }
     });
 });
 
-function updateFormSteps(){
+prevBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+        e.preventDefault();
+        if (formStepsNum > 0) {
+            formStepsNum--;
+            updateFormSteps();
+            updateProgressBar();
+        }
+    });
+});
 
-    formSteps.forEach(formSteps=>{
-        formSteps.classList.contains("form-step-active") &&
-        formSteps.classList.remove("form-step-active");
-
-    })
-
+function updateFormSteps() {
+    formSteps.forEach((formStep, index) => {
+        formStep.classList.remove("form-step-active");
+    });
     formSteps[formStepsNum].classList.add("form-step-active");
 }
 
+function updateProgressBar() {
+    progressSteps.forEach((step, idx) => {
+        if (idx <= formStepsNum) {
+            step.classList.add("progress-step-active");
+        } else {
+            step.classList.remove("progress-step-active");
+        }
+    });
+
+    const progressPercentage = (formStepsNum) / (progressSteps.length - 1) * 100;
+    progress.style.width = `${progressPercentage}%`;
+}
+
+progressSteps.forEach((step, index) => {
+  step.addEventListener("click", () => {
+    formStepsNum = index;
+    updateFormSteps();
+    updateProgressBar();
+  });
+});
